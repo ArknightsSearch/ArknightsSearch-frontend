7a5a5c7253f65ddfb24b809162b0d063276a73d8
meeboo3 <meeboo3@qq.com>
2023-05-17 21:55:41 +0800

    feat TextExtraData增加多段

--- src/components/extra/TextData.vue
+++ src/components/extra/TextData.vue
@@ -5,10 +5,19 @@ const props = defineProps(['data'])
 
 <template>
     <div style="margin: 13px 0;">
-        <p style="position: relative; left: -2%; margin: 5px 0"><small><strong>文本：{{props.data.raw}}</strong></small></p>
-        <p v-if="props.data.data[0]" style="">{{ props.data.data[0] }}</p>
-        <p style="margin: 0">{{ props.data.data[1] }}<b>{{ props.data.data[2] }}</b>{{ props.data.data[3] }}</p>
-        <p v-if="props.data.data[4]" style="">{{ props.data.data[4] }}</p>
+        <p style="position: relative; left: -2%; margin: 5px 0"><small><strong>
+            文本：{{ props.data.raw }}</strong></small></p>
+        <div v-for="(data, index) in props.data.data" :key="index" >
+            <el-divider style="margin: 10px 0" border-style="dashed" v-if="index!==0"/>
+            <p v-if="data[0]" style="">{{ data[0] }}</p>
+            <p style="margin: 0">{{ data[1] }}<b>{{ data[2] }}</b>{{ data[3] }}</p>
+            <p v-if="data[4]" style="">{{ data[4] }}</p>
+        </div>
+        <p v-if="props.data.has_more">
+            <el-divider style="margin: 10px 0" border-style="dashed"/>
+            ...
+        </p>
+
     </div>
 </template>
 
