3fb55755b14050e9ffc3af2a441256207b5d43e6
meeboo3 <meeboo3@qq.com>
2023-06-08 12:24:54 +0800

    optimize 适配仅立绘出场角色

--- src/components/extra/CharExtra.vue
+++ src/components/extra/CharExtra.vue
@@ -5,9 +5,15 @@ const props = defineProps(['data'])
 
 <template>
     <div style="margin: 13px 0">
-        <p style="position: relative; left: -2%; margin: 5px 0"><small><strong>角色：{{props.data.raw}}</strong></small></p>
-        <p v-for="(text, index) in props.data.data" :key="index">{{ text }}</p>
-        <p v-if="props.data.has_more">...</p>
+        <p style="position: relative; left: -2%; margin: 5px 0">
+            <small><strong>角色：{{ props.data.raw }}</strong></small></p>
+        <template v-if="props.data.data.length">
+            <p v-for="(text, index) in props.data.data" :key="index">{{ text }}</p>
+            <p v-if="props.data.has_more">...</p>
+        </template>
+        <template v-else>
+            <p><i>仅立绘出场，无剧情</i></p>
+        </template>
     </div>
 </template>
 
